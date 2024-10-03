bd04701f2e9a38e8c9c7ad8e3c07db3ce741cd15
deepdeb <debnath.pradeep619@gmail.com>
2024-10-03 11:28:49 +0530

    modify files

--- src/api/services/getDesignationByIdService.js
+++ src/api/services/getDesignationByIdService.js
@@ -2,7 +2,7 @@ const readPool = require('../../config/mysql').readPool
 
 exports.getDesignationById = async (data) => {
     try {
-        let get_designation_by_id_sql = "SELECT designation_id, designation_name FROM designation WHERE designation_id = ?"
+        let get_designation_by_id_sql = "SELECT designation_id, designation_name FROM designation WHERE designation_id = ? AND is_active = 1 AND is_deleted = 0"
         const get_designation_by_id_resp = await readPool.query(get_designation_by_id_sql, [data.designation_id])
 
         if(get_designation_by_id_resp) {
