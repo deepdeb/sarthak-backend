f16fd0d341baa83e4d0c02044bbcd5fc99b3e145
deepdeb <debnath.pradeep619@gmail.com>
2024-10-04 17:20:15 +0530

    modify files

--- src/api/services/getMentorListService.js
+++ src/api/services/getMentorListService.js
@@ -2,7 +2,7 @@ const readPool = require('../../config/mysql').readPool;
 
 exports.getMentorList = async (data) => {
     try {
-        let mentor_list_sql = "SELECT m.mentor_id, m.sales_person_id, sp.sales_person_name, m.sbu_id, sb.sbu_name FROM mentor m JOIN sales_person sp ON sp.sales_person_id = m.sales_person_id JOIN sbu sb ON sb.sbu_id = m.sbu_id WHERE m.sbu_id = ?"
+        let mentor_list_sql = "SELECT m.mentor_id, m.sales_person_id, sp.sales_person_name as mentor_name, m.sbu_id, sb.sbu_name FROM mentor m JOIN sales_person sp ON sp.sales_person_id = m.sales_person_id JOIN sbu sb ON sb.sbu_id = m.sbu_id WHERE m.sbu_id = ?"
         const [mentor_list_resp] = await readPool.query(mentor_list_sql, [data.sbu_id]);
 
         if(mentor_list_resp) {
