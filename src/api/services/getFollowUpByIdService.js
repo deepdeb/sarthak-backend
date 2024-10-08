4ceb5b7658203ed277115cb7df7e5ed1d151c492
deepdeb <debnath.pradeep619@gmail.com>
2024-10-08 12:31:22 +0530

    modify getFollowUpByIdService

--- src/api/services/getFollowUpByIdService.js
+++ src/api/services/getFollowUpByIdService.js
@@ -2,7 +2,7 @@ const readPool = require('../../config/mysql').readPool;
 
 exports.getFollowUpById = async (data) => {
     try {
-        let follow_up_select_sql = "SELECT follow_up_id, enquiry_id, DATA_FORMAT(status_date, '%Y-%m-%d') as status_date, status, remarks, support FROM follow_up WHERE enquiry_id = ?"
+        let follow_up_select_sql = "SELECT follow_up_id, enquiry_id, DATE_FORMAT(status_date, '%Y-%m-%d') as status_date, status, remarks, support FROM follow_up WHERE enquiry_id = ?"
         const [follow_up_select_resp] = await readPool.query(follow_up_select_sql, [data.enquiry_id]);
         if(follow_up_select_resp) {
             return follow_up_select_resp;
