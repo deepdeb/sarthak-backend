1aee0e8fbbf684e05870c9dc1bd9d3a1a537e305
deepdeb <debnath.pradeep619@gmail.com>
2024-10-18 18:11:58 +0530

    modify getSalespersonListService.js

--- src/api/services/getSalespersonListService.js
+++ src/api/services/getSalespersonListService.js
@@ -11,7 +11,7 @@ exports.getSalespersonList = async (data) => {
             resp.push(select_resp, total_count);
             return resp;
         } else if (data.sbu_id == 1) {
-            let sql = "SELECT sp.sales_person_id, sp.sbu_id, s.sbu_name, f.function_name, sp.sales_person_name, d.designation_name, sp.mobile, sp.email, DATE_FORMAT(sp.dob, '%d-%m-%Y') as dob, sp.password FROM sales_person sp JOIN sbu s ON s.sbu_id = sp.sbu_id JOIN functions f ON f.function_id = sp.function_id JOIN designation d ON sp.designation_id = d.designation_id WHERE sp.designation_id = 4 AND sp.sbu_id = ? ORDER BY sp.sales_person_id DESC"
+            let sql = "SELECT sp.sales_person_id, sp.sbu_id, s.sbu_name, f.function_name, sp.sales_person_name, d.designation_name, sp.mobile, sp.email, DATE_FORMAT(sp.dob, '%d-%m-%Y') as dob, sp.password FROM sales_person sp JOIN sbu s ON s.sbu_id = sp.sbu_id JOIN functions f ON f.function_id = sp.function_id JOIN designation d ON sp.designation_id = d.designation_id WHERE (sp.designation_id = 4 OR sp.designation_id = 3 OR sp.designation_id = 2) AND sp.sbu_id = ? ORDER BY sp.sales_person_id DESC"
             const [select_resp] = await readPool.query(sql, [data.sbu_id]);
 
             const total_count = select_resp.length
