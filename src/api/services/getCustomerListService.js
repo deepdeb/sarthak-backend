c78ac23f3c939820e86cdd6c2df989792a88e343
deepdeb <debnath.pradeep619@gmail.com>
2024-10-23 13:48:26 +0530

    modify getCustomerListService.js

--- src/api/services/getCustomerListService.js
+++ src/api/services/getCustomerListService.js
@@ -2,37 +2,24 @@ const readPool = require('../../config/mysql').readPool;
 
 exports.getCustomerList = async (data) => {
     try {
-        let resp_to_send = []
-        if(data.check_designation_id == 1) {
-            if(data.sbu_id == 0) {
-                let sql_1 = "SELECT c.customer_id, c.sbu_id, sb.sbu_name, c.sales_person_id, DATE_FORMAT(c.customer_create_date, '%Y-%m-%d') as customer_create_date, sp.sales_person_name, sp.mentor_id, m.mentor_name, c.customer, s.segment_name, ss.subsegment_name, c.name, c.designation, c.department, c.mobile, c.alt_mobile, c.email, c.alt_email, c.product_category_id, DATE_FORMAT(c.created_at, '%d-%m-%Y') as date, a.street_no, a.street_name, a.area, a.location, a.district, a.city, st.state_name, a.pin FROM customer c JOIN segment s ON s.segment_id = c.segment_id LEFT JOIN subsegment ss ON ss.subsegment_id = c.subsegment_id JOIN sbu sb ON sb.sbu_id = c.sbu_id JOIN address a ON a.customer_id = c.customer_id JOIN state st ON st.state_id = a.state_id JOIN sales_person sp ON sp.sales_person_id = c.sales_person_id LEFT JOIN mentor m ON m.mentor_id = sp.mentor_id ORDER BY c.customer_id DESC"
-                const [resp_1] = await readPool.query(sql_1)
+        if (data.check_designation_id == 1) {
+            let sql = "SELECT c.customer_id, c.sbu_id, sb.sbu_name, c.sales_person_id, DATE_FORMAT(c.customer_create_date, '%Y-%m-%d') as customer_create_date, sp.sales_person_name, c.customer, s.segment_name, ss.subsegment_name, c.name, c.designation, c.department, c.mobile, c.alt_mobile, c.email, c.alt_email, c.product_category_id, DATE_FORMAT(c.created_at, '%d-%m-%Y') as date, a.street_no, a.street_name, a.area, a.location, a.district, a.city, st.state_name, a.pin FROM customer c JOIN segment s ON s.segment_id = c.segment_id LEFT JOIN subsegment ss ON ss.subsegment_id = c.subsegment_id JOIN sbu sb ON sb.sbu_id = c.sbu_id JOIN address a ON a.customer_id = c.customer_id JOIN state st ON st.state_id = a.state_id JOIN sales_person sp ON sp.sales_person_id = c.sales_person_id ORDER BY c.customer_id DESC"
+            const [resp] = await readPool.query(sql)
 
-                let total_count = resp_1.length;
+            let total_count = resp.length;
 
-                resp_to_send.push(resp_1, total_count);
-                return resp_to_send;
-            } else {
-                let sql_2 = "SELECT c.customer_id, c.sbu_id, sb.sbu_name, c.sales_person_id, DATE_FORMAT(c.customer_create_date, '%Y-%m-%d') as customer_create_date, sp.sales_person_name, sp.mentor_id, m.mentor_name, c.customer, s.segment_name, ss.subsegment_name, c.name, c.designation, c.department, c.mobile, c.alt_mobile, c.email, c.alt_email, c.product_category_id, DATE_FORMAT(c.created_at, '%d-%m-%Y') as date, a.street_no, a.street_name, a.area, a.location, a.district, a.city, st.state_name, a.pin FROM customer c JOIN segment s ON s.segment_id = c.segment_id LEFT JOIN subsegment ss ON ss.subsegment_id = c.subsegment_id JOIN sbu sb ON sb.sbu_id = c.sbu_id JOIN address a ON a.customer_id = c.customer_id JOIN state st ON st.state_id = a.state_id JOIN sales_person sp ON sp.sales_person_id = c.sales_person_id LEFT JOIN mentor m ON m.mentor_id = sp.mentor_id WHERE c.sbu_id = ? ORDER BY c.customer_id DESC"
-                const [resp_2] = await readPool.query(sql_2, [data.sbu_id]);
-
-                let total_count = resp_2.length;
-                
-                resp_to_send.push(resp_2, total_count);
-                return resp_to_send;
-            }
+            return [resp, total_count];
         }
         else {
-            let sql_3 = "SELECT c.customer_id, c.sbu_id, sb.sbu_name, c.sales_person_id, DATE_FORMAT(c.customer_create_date, '%Y-%m-%d') as customer_create_date, c.mentor_id, sp.sales_person_name, sp.mentor_id, m.mentor_name, c.customer, s.segment_name, ss.subsegment_name, c.name, c.designation, c.department, c.mobile, c.alt_mobile, c.email, c.alt_email, c.product_category_id, DATE_FORMAT(c.created_at, '%d-%m-%Y') as date, a.street_no, a.street_name, a.area, a.location, a.district, a.city, st.state_name, a.pin FROM customer c JOIN segment s ON s.segment_id = c.segment_id LEFT JOIN subsegment ss ON ss.subsegment_id = c.subsegment_id JOIN sbu sb ON sb.sbu_id = c.sbu_id JOIN address a ON a.customer_id = c.customer_id JOIN state st ON st.state_id = a.state_id JOIN sales_person sp ON sp.sales_person_id = c.sales_person_id LEFT JOIN mentor m ON m.mentor_id = sp.mentor_id WHERE c.sbu_id =? AND c.sales_person_id = ? ORDER BY c.customer_id DESC"
-            const [resp_3] = await readPool.query(sql_3, [data.sbu_id, data.sales_person_id]);
+            let sql = "SELECT c.customer_id, c.sbu_id, sb.sbu_name, c.sales_person_id, DATE_FORMAT(c.customer_create_date, '%Y-%m-%d') as customer_create_date, sp.sales_person_name, c.customer, s.segment_name, ss.subsegment_name, c.name, c.designation, c.department, c.mobile, c.alt_mobile, c.email, c.alt_email, c.product_category_id, DATE_FORMAT(c.created_at, '%d-%m-%Y') as date, a.street_no, a.street_name, a.area, a.location, a.district, a.city, st.state_name, a.pin FROM customer c JOIN segment s ON s.segment_id = c.segment_id LEFT JOIN subsegment ss ON ss.subsegment_id = c.subsegment_id JOIN sbu sb ON sb.sbu_id = c.sbu_id JOIN address a ON a.customer_id = c.customer_id JOIN state st ON st.state_id = a.state_id JOIN sales_person sp ON sp.sales_person_id = c.sales_person_id WHERE c.sbu_id =? AND c.sales_person_id = ? ORDER BY c.customer_id DESC"
+            const [resp] = await readPool.query(sql, [data.sbu_id, data.sales_person_id]);
 
-            let total_count = resp_3.length;
+            let total_count = resp.length;
 
-            resp_to_send.push(resp_3, total_count);
-            return resp_to_send;
+            return [resp, total_count];
         }
     } catch (error) {
-        console.log('Error: ', error)
+        console.log('Get customer list service error: ', error)
         return;
     }
 }
