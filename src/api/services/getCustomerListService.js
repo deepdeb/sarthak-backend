e09434aed906fde038fcac488561be6bcd19c7a4
deepdeb <debnath.pradeep619@gmail.com>
2024-10-24 11:52:07 +0530

    modify getCustomerListService.js

--- src/api/services/getCustomerListService.js
+++ src/api/services/getCustomerListService.js
@@ -2,7 +2,7 @@ const readPool = require('../../config/mysql').readPool;
 
 exports.getCustomerList = async (data) => {
     try {
-        if (data.sbu_id == 1) {
+        if (data.sbu_id == 0) {
             let sql = "SELECT c.customer_id, c.sbu_id, sb.sbu_name, c.sales_person_id, DATE_FORMAT(c.customer_create_date, '%Y-%m-%d') as customer_create_date, sp.sales_person_name, c.customer, s.segment_name, ss.subsegment_name, c.name, c.designation, c.department, c.mobile, c.alt_mobile, c.email, c.alt_email, c.product_category_id, DATE_FORMAT(c.created_at, '%d-%m-%Y') as date, a.street_no, a.street_name, a.area, a.location, a.district, a.city, st.state_name, a.pin FROM customer c JOIN segment s ON s.segment_id = c.segment_id LEFT JOIN subsegment ss ON ss.subsegment_id = c.subsegment_id JOIN sbu sb ON sb.sbu_id = c.sbu_id JOIN address a ON a.customer_id = c.customer_id JOIN state st ON st.state_id = a.state_id JOIN sales_person sp ON sp.sales_person_id = c.sales_person_id ORDER BY c.customer_id DESC"
             const [resp] = await readPool.query(sql)
 
