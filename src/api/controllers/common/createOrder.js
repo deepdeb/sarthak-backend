735a34516e9eaeb6568e836236805f0da918adbb
deepdeb <debnath.pradeep619@gmail.com>
2024-10-24 18:12:22 +0530

    modify files

--- src/api/controllers/common/createOrder.js
+++ src/api/controllers/common/createOrder.js
@@ -43,3 +43,48 @@ exports.createOrderController = async (req, res) => {
         return
     }
 }
+
+exports.editOrderController = async (req, res) => {
+    try {
+        const editOrderData = Joi.object({
+            order_id: Joi.required(),
+            sales_person_id: Joi.required(),
+            sbu_id: Joi.required(),
+            customer_id: Joi.required(),
+            po_number: Joi.required(),
+            po_date: Joi.required(),
+            po_type_id: Joi.required(),
+            product: Joi.optional(),
+            description: Joi.optional(),
+            brand: Joi.optional(),
+            cable_assembly: Joi.optional(),
+            panel: Joi.optional(),
+            welding_receptable: Joi.optional(),
+            clamps: Joi.optional(),
+            hsa_box: Joi.optional(),
+            others: Joi.optional(),
+            basic_po_value: Joi.required(),
+            total_po_value: Joi.required(),
+            scheduled_completion_date: Joi.required(),
+            actual_completion_date: Joi.optional(),
+            purchase_order_file: Joi.optional(),
+            completion_file: Joi.optional(),
+            credential_file: Joi.optional()
+        })
+        const { error, value } = editOrderData.validate(req.body);
+        if(error) {
+            console.log(`Invalid edit order data: ${error.details[0].message}`);
+            return res.status(400).json({ success: false, message: error.details[0].message.replace(/["':]/g, '') });
+        }
+        console.log(`Valid edit order data`);
+        const resp = await createOrderService.editOrder(req.body);
+        if(resp) {
+            return res.json({success: true, status: 200, response: resp, message: resp});
+        } else {
+            return res.json({success: false, status: 500, response: []});
+        }
+    } catch (error) {
+        console.log('Edit order controller error: ', error);
+        return
+    }
+}
--- src/api/services/createOrderService.js
+++ src/api/services/createOrderService.js
@@ -11,3 +11,16 @@ exports.createOrder = async (data) => {
         return;
     }
 }
+
+exports.editOrder = async (data) => {
+    try {
+        let edit_order_sql = "UPDATE order SET sales_person_id = ?, sbu_id = ?, customer_id = ?, po_number = ?, po_date = ?, po_type_id = ?, product = ?, description = ?, brand = ?, cable_assembly = ?, panel = ?, welding_receptable = ?, clamps = ?, hsa_box = ?, others = ?, basic_po_value = ?, total_po_value = ?, scheduled_completion_date = ?, actual_completion_date = ?, purchase_order_file = ?, completion_file = ?, credential_file = ? WHERE order_id = ?"
+        const [resp] = await writePool.query(edit_order_sql, [data.sales_person_id, data.sbu_id, data.customer_id, data.po_number, data.po_date, data.po_type_id, data.product, data.description, data.brand, data.cable_assembly, data.panel, data.welding_receptable, data.clamps, data.hsa_box, data.others, data.basic_po_value, data.total_po_value, data.scheduled_completion_date, data.actual_completion_date, data.purchase_order_file, data.completion_file, data.credential_file, data.order_id])
+        if(resp) {
+            return 'Order edited successfully'
+        }
+    } catch (error) {
+        console.log('Edit order service error:', error);
+        return;
+    }
+}
