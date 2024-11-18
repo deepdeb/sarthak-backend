ce037d22da576db75b65fdd0dede782894a6cb8d
deepdeb <debnath.pradeep619@gmail.com>
2024-11-18 15:58:27 +0530

    modify enquiryReportService.js

--- src/api/services/enquiryReportService.js
+++ src/api/services/enquiryReportService.js
@@ -13,7 +13,7 @@ exports.getEnquiryReport = async (data) => {
             searchCondition2 += " AND e.enquiry_date BETWEEN '" + data.start_date + "' AND '" + data.end_date + "'"
         }
 
-        let sql = "SELECT e.enquiry_id, e.customer_id, c.customer, DATE_FORMAT(e.enquiry_date, '%Y-%m-%d') as enquiry_date, e.enquiry_source, e.enquiry_sub_type_id, est.enquiry_sub_type_name, e.principal_house, e.offer_date, e.basic_value, e.tentative_finalization_month, e.tentative_finalization_year, f.follow_up_id, f.status FROM enquiry e JOIN customer c ON c.customer_id = e.customer_id JOIN enquiry_sub_type est ON est.enquiry_sub_type_id = e.enquiry_sub_type_id LEFT JOIN follow_up f ON f.enquiry_id = e.enquiry_id" + searchCondition + searchCondition2 + ""
+        let sql = "SELECT e.enquiry_id, e.customer_id, c.customer, DATE_FORMAT(e.enquiry_date, '%Y-%m-%d') as enquiry_date, e.enquiry_source, e.enquiry_sub_type_id, est.enquiry_sub_type_name, e.principal_house, DATE_FORMAT(e.offer_date, '%Y-%m-%d') as offer_date, e.basic_value, e.tentative_finalization_month, e.tentative_finalization_year, f.follow_up_id, f.status FROM enquiry e JOIN customer c ON c.customer_id = e.customer_id JOIN enquiry_sub_type est ON est.enquiry_sub_type_id = e.enquiry_sub_type_id LEFT JOIN follow_up f ON f.enquiry_id = e.enquiry_id" + searchCondition + searchCondition2 + ""
 
         const [resp] = await readPool.query(sql);
 
