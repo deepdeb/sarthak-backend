681a636a40c3d87d2cd9d9d0f5a55d70b4bb2ef6
deepdeb <debnath.pradeep619@gmail.com>
2024-11-27 15:42:51 +0530

    modify getCustomerListForEnquiryOrderService.js

--- src/api/services/getCustomerListForEnquiryOrderService.js
+++ src/api/services/getCustomerListForEnquiryOrderService.js
@@ -4,24 +4,24 @@ exports.getCustomerListForEnquiryOrder = async (data) => {
     try {
         if (data.sbu_id == 0) {
             if(data.report_navigation_type == 'order') {
-                let sql = "SELECT c.customer_id, c.customer, o.customer_id FROM customer c INNER JOIN orders o ON o.customer_id = c.customer_id WHERE o.is_deleted = 0 AND o.is_active = 1 AND c.is_deleted = 0 AND c.is_active = 1";
+                let sql = "SELECT c.customer_id, c.customer FROM customer c INNER JOIN orders o ON o.customer_id = c.customer_id WHERE o.is_deleted = 0 AND o.is_active = 1 AND c.is_deleted = 0 AND c.is_active = 1 GROUP BY c.customer_id";
                 const [resp] = await readPool.query(sql)
 
                 return resp;
             } else if (data.report_navigation_type == 'enquiry') {
-                let sql = "SELECT c.customer_id, c.customer, e.customer_id FROM customer c INNER JOIN enquiry e ON e.customer_id = c.customer_id WHERE e.is_deleted = 0 AND e.is_active = 1 AND e.is_deleted = 0 AND e.is_active = 1";
+                let sql = "SELECT c.customer_id, c.customer FROM customer c INNER JOIN enquiry e ON e.customer_id = c.customer_id WHERE e.is_deleted = 0 AND e.is_active = 1 AND e.is_deleted = 0 AND e.is_active = 1 GROUP BY c.customer_id";
                 const [resp] = await readPool.query(sql)
 
                 return resp;
             }
         } else {
             if(data.report_navigation_type == 'order') {
-                let sql = "SELECT c.customer_id, c.customer, o.customer_id FROM customer c INNER JOIN orders o ON o.customer_id = c.customer_id WHERE o.is_deleted = 0 AND o.is_active = 1 AND c.is_deleted = 0 AND c.is_active = 1 AND c.sales_person_id = ?";
+                let sql = "SELECT c.customer_id, c.customer FROM customer c INNER JOIN orders o ON o.customer_id = c.customer_id WHERE o.is_deleted = 0 AND o.is_active = 1 AND c.is_deleted = 0 AND c.is_active = 1 AND c.sales_person_id = ? GROUP BY c.customer_id";
                 const [resp] = await readPool.query(sql, [data.sales_person_id]);
 
                 return resp;
             } else if (data.report_navigation_type == 'enquiry') {
-                let sql = "SELECT c.customer_id, c.customer, e.customer_id FROM customer c INNER JOIN enquiry e ON e.customer_id = c.customer_id WHERE e.is_deleted = 0 AND e.is_active = 1 AND e.is_deleted = 0 AND e.is_active = 1 AND c.sales_person_id = ?";
+                let sql = "SELECT c.customer_id, c.customer FROM customer c INNER JOIN enquiry e ON e.customer_id = c.customer_id WHERE e.is_deleted = 0 AND e.is_active = 1 AND e.is_deleted = 0 AND e.is_active = 1 AND c.sales_person_id = ? GROUP BY c.customer_id";
                 const [resp] = await readPool.query(sql, [data.sales_person_id]);
 
                 return resp;
