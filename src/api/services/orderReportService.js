fad3d7b83f9d1087d9b940eb5f368ee446d76b2b
deepdeb <debnath.pradeep619@gmail.com>
2024-11-27 11:30:45 +0530

    modify files

--- src/api/services/orderReportService.js
+++ src/api/services/orderReportService.js
@@ -15,7 +15,7 @@ exports.getOrderReport = async (data) => {
             searchCondition2 += " AND o.po_date BETWEEN '" + data.start_date + "' AND '" + data.end_date + "'"
         }
 
-        let sql = "SELECT o.customer_id, c.customer, DATE_FORMAT(o.po_date, '%Y-%m-%d') as po_date, o.po_number, o.po_type_id, p.po_type_name, o.po_subtype_id, post.po_subtype_name, o.basic_po_value, o.total_po_value, DATE_FORMAT(o.scheduled_completion_date, '%Y-%m-%d') as scheduled_completion_date, DATE_FORMAT(o.actual_completion_date, '%Y-%m-%d') as actual_completion_date FROM orders o JOIN customer c ON o.customer_id = c.customer_id JOIN po_type p ON p.po_type_id = o.po_type_id LEFT JOIN po_subtype post ON post.po_subtype_id = o.po_subtype_id" + searchCondition + searchCondition2 + ""
+        let sql = "SELECT o.customer_id, c.customer, DATE_FORMAT(o.po_date, '%d-%m-%Y') as po_date, o.po_number, o.po_type_id, p.po_type_name, o.po_subtype_id, post.po_subtype_name, o.basic_po_value, o.total_po_value, DATE_FORMAT(o.scheduled_completion_date, '%d-%m-%Y') as scheduled_completion_date, DATE_FORMAT(o.actual_completion_date, '%d-%m-%Y') as actual_completion_date FROM orders o JOIN customer c ON o.customer_id = c.customer_id JOIN po_type p ON p.po_type_id = o.po_type_id LEFT JOIN po_subtype post ON post.po_subtype_id = o.po_subtype_id" + searchCondition + searchCondition2 + ""
         const [resp] = await readPool.query(sql);
         if(resp) {
             return resp;
