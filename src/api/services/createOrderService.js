44ec3db632eb1101a6a2af06c00ddb0b9ba98b0b
deepdeb <debnath.pradeep619@gmail.com>
2024-11-30 01:04:00 +0530

    modify files

--- src/api/services/createOrderService.js
+++ src/api/services/createOrderService.js
@@ -14,8 +14,8 @@ exports.createOrder = async (data) => {
 
 exports.editOrder = async (data) => {
     try {
-        let edit_order_sql = "UPDATE orders SET sales_person_id = ?, sbu_id = ?, customer_id = ?, po_number = ?, po_date = ?, po_type_id = ?, product = ?, supply_description = ?, brand = ?, sitc_description = ?, cable_assembly = ?, panel = ?, welding_receptable = ?, clamps = ?, hsa_box = ?, others = ?, basic_po_value = ?, total_po_value = ?, scheduled_completion_date = ?, actual_completion_date = ?, purchase_order_file = ?, completion_file = ?, credential_file = ? WHERE order_id = ?"
-        const [resp] = await writePool.query(edit_order_sql, [data.sales_person_id, data.sbu_id, data.customer_id, data.po_number, data.po_date, data.po_type_id, data.product, data.supply_description, data.brand, data.sitc_description, data.cable_assembly, data.panel, data.welding_receptable, data.clamps, data.hsa_box, data.others, data.basic_po_value, data.total_po_value, data.scheduled_completion_date, data.actual_completion_date, data.purchase_order_file, data.completion_file, data.credential_file, data.order_id])
+        let edit_order_sql = "UPDATE orders SET po_type_id = ?, product = ?, supply_description = ?, brand = ?, sitc_description = ?, cable_assembly = ?, panel = ?, welding_receptable = ?, clamps = ?, hsa_box = ?, others = ?, basic_po_value = ?, total_po_value = ?, scheduled_completion_date = ?, actual_completion_date = ?, purchase_order_file = ?, completion_file = ?, credential_file = ? WHERE order_id = ?"
+        const [resp] = await writePool.query(edit_order_sql, [data.po_type_id, data.product, data.supply_description, data.brand, data.sitc_description, data.cable_assembly, data.panel, data.welding_receptable, data.clamps, data.hsa_box, data.others, data.basic_po_value, data.total_po_value, data.scheduled_completion_date, data.actual_completion_date, data.purchase_order_file, data.completion_file, data.credential_file, data.order_id])
         if(resp) {
             return 'Order edited successfully'
         }
