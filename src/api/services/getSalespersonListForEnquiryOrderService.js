f76eaa162ad958304f9348b4f450e035a4c795ff
deepdeb <debnath.pradeep619@gmail.com>
2024-12-02 13:45:21 +0530

    modify files

--- src/api/services/getSalespersonListForEnquiryOrderService.js
+++ src/api/services/getSalespersonListForEnquiryOrderService.js
@@ -3,24 +3,24 @@ const readPool = require('../../config/mysql').readPool;
 exports.getSalespersonListForEnquiryOrder = async (data) => {
     try {
         if (data.sbu_id == 0) {
-            if(data.report_navigation_type == 'order') {
+            if(data.report_navigation_type == 'sales_order') {
                 let sql = "SELECT sp.sales_person_id, sp.sales_person_name FROM sales_person sp INNER JOIN orders o ON o.sales_person_id = sp.sales_person_id WHERE o.is_deleted = 0 AND o.is_active = 1 AND sp.is_deleted = 0 GROUP BY sp.sales_person_id";
                 const [resp] = await readPool.query(sql)
 
                 return resp;
-            } else if (data.report_navigation_type == 'enquiry') {
+            } else if (data.report_navigation_type == 'sales_enquiry') {
                 let sql = "SELECT sp.sales_person_id, sp.sales_person_name FROM sales_person sp INNER JOIN enquiry e ON e.sales_person_id = sp.sales_person_id WHERE e.is_deleted = 0 AND e.is_active = 1 AND sp.is_deleted = 0 GROUP BY sp.sales_person_id";
                 const [resp] = await readPool.query(sql)
 
                 return resp;
             }
         } else {
-            if(data.report_navigation_type == 'order') {
+            if(data.report_navigation_type == 'sales_order') {
                 let sql = "SELECT sp.sales_person_id, sp.sales_person_name FROM sales_person sp INNER JOIN orders o ON o.sales_person_id = sp.sales_person_id WHERE o.is_deleted = 0 AND o.is_active = 1 AND sp.is_deleted = 0 AND sp.sales_person_id = ? GROUP BY sp.sales_person_id";
                 const [resp] = await readPool.query(sql, [data.sales_person_id]);
 
                 return resp;
-            } else if (data.report_navigation_type == 'enquiry') {
+            } else if (data.report_navigation_type == 'sales_enquiry') {
                 let sql = "SELECT sp.sales_person_id, sp.sales_person_name FROM sales_person sp INNER JOIN enquiry e ON e.sales_person_id = sp.sales_person_id WHERE e.is_deleted = 0 AND e.is_active = 1 AND sp.is_deleted = 0 AND sp.sales_person_id = ? GROUP BY sp.sales_person_id";
                 const [resp] = await readPool.query(sql, [data.sales_person_id]);
 
