f6ba6dba18614d11e3f2d2a91dbf168cd3c19206
deepdeb <debnath.pradeep619@gmail.com>
2024-12-03 14:59:11 +0530

    modify files

--- src/api/services/getCompanyByIdService.js
+++ src/api/services/getCompanyByIdService.js
@@ -1,7 +1,7 @@
 const { readPool } = require('../../config/mysql');
 exports.getCompanyById = async (data) => {
     try {
-        let get_company_by_id_sql = "SELECT sb.sbu_name, sb.contact_person, sb.contact_number, sb.email, sb.address, sb.city, st.state_id, sb.pin FROM sbu sb JOIN state st ON st.state_id = sb.state_id WHERE sb.sbu_id = ?"
+        let get_company_by_id_sql = "SELECT sb.sbu_name, sb.contact_person, sb.contact_number, sb.email, sb.address, sb.city, sb.state_id, st.state_name, sb.pin FROM sbu sb JOIN state st ON st.state_id = sb.state_id WHERE sb.sbu_id = ?"
         const [get_company_by_id_resp] = await readPool.query(get_company_by_id_sql, [data.sbu_id]);
 
         if(get_company_by_id_resp) {
--- src/api/services/getSBUlistService.js
+++ src/api/services/getSBUlistService.js
@@ -3,13 +3,13 @@ const readPool = require('../../config/mysql').readPool;
 exports.getSBUlist = async (data) => {
     try {
         if(data.sbu_id == 0) {
-            let SBU_list_sql = "SELECT sb.sbu_id, sb.sbu_name, sb.contact_person, sb.contact_number, sb.email, sb.address, sb.city, st.state_name, sb.pin FROM sbu sb JOIN state st ON sb.state_id = st.state_id";
+            let SBU_list_sql = "SELECT sb.sbu_id, sb.sbu_name, sb.contact_person, sb.contact_number, sb.email, sb.address, sb.city, sb.state_id, st.state_name, sb.pin FROM sbu sb JOIN state st ON sb.state_id = st.state_id";
 
             const [resp] = await readPool.query(SBU_list_sql);
     
             return resp;
         } else {
-            let SBU_list_sql = "SELECT sb.sbu_id, sb.sbu_name, sb.contact_person, sb.contact_number, sb.email, sb.address, sb.city, st.state_name, sb.pin FROM sbu sb JOIN state st ON sb.state_id = st.state_id WHERE sb.sbu_id = ?";
+            let SBU_list_sql = "SELECT sb.sbu_id, sb.sbu_name, sb.contact_person, sb.contact_number, sb.email, sb.address, sb.city, sb.state_id, st.state_name, sb.pin FROM sbu sb JOIN state st ON sb.state_id = st.state_id WHERE sb.sbu_id = ?";
 
             const [resp] = await readPool.query(SBU_list_sql, [data.sbu_id]);
     
