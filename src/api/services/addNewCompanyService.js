795d6bc43ee404d5af7390d9d9b7dc4e4484a871
deepdeb <debnath.pradeep619@gmail.com>
2024-12-04 14:37:51 +0530

    modify files

--- src/api/services/addNewCompanyService.js
+++ src/api/services/addNewCompanyService.js
@@ -1,12 +1,21 @@
 const writePool = require('../../config/mysql').writePool
+const readPool = require('../../config/mysql').readPool
+
 
 exports.addNewCompany = async (data) => {
     try {
-        let add_company_sql = "INSERT into sbu (sbu_name, contact_person, contact_number, email, address, city, state_id, pin) VALUES (?,?,?,?,?,?,?,?)"
-        const [add_company_resp] = await writePool.query(add_company_sql, [data.sbu_name, data.contact_person, data.contact_number, data.email, data.address, data.city, data.state_id, data.pin]);
+        let company_exist_sql = "SELECT count(sbu_id) as count FROM sbu WHERE sbu_name = ? AND is_deleted = 0 AND is_active = 1"
+        const [company_exist_resp] = await readPool.query(company_exist_sql, [data.sbu_name])
 
-        if(add_company_resp) {
-            return 'Company added successfully'
+        if (company_exist_resp[0].count == 0) {
+            let add_company_sql = "INSERT into sbu (sbu_name, contact_person, contact_number, email, address, city, state_id, pin) VALUES (?,?,?,?,?,?,?,?)"
+            const [add_company_resp] = await writePool.query(add_company_sql, [data.sbu_name, data.contact_person, data.contact_number, data.email, data.address, data.city, data.state_id, data.pin]);
+
+            if (add_company_resp) {
+                return 'Company added successfully'
+            }
+        } else {
+            return 'Company already exists'
         }
     } catch (error) {
         console.log('add new company service error:', error);
@@ -19,7 +28,7 @@ exports.editCompany = async (data) => {
         let edit_company_sql = "UPDATE sbu SET sbu_name = ?, contact_person = ?, contact_number = ?, email = ?, address = ?, city = ?, state_id = ?, pin = ? WHERE sbu_id = ?"
         const [edit_company_resp] = await writePool.query(edit_company_sql, [data.sbu_name, data.contact_person, data.contact_number, data.email, data.address, data.city, data.state_id, data.pin, data.sbu_id]);
 
-        if(edit_company_resp) {
+        if (edit_company_resp) {
             return 'Company edited successfully'
         }
     } catch (error) {
