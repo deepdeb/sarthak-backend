becf16655a834702ea5899e68377da08c9321a29
deepdeb <debnath.pradeep619@gmail.com>
2024-12-05 16:43:49 +0530

    modify files

--- src/api/controllers/common/getOrderList.js
+++ src/api/controllers/common/getOrderList.js
@@ -5,7 +5,8 @@ exports.getOrderListController = async (req, res) => {
         const getOrderListData = Joi.object({
             sbu_id: Joi.required(),
             sales_person_id: Joi.required(),
-            search_criteria: Joi.optional()
+            search_criteria: Joi.optional(),
+            order_offset: Joi.optional()
         })
         const { error, value } = getOrderListData.validate(req.body);
         if(error) {
@@ -15,7 +16,7 @@ exports.getOrderListController = async (req, res) => {
         console.log(`Valid get order list data`);
         const resp = await getOrderListService.getOrderList(req.body);
         if(resp) {
-            return res.json({ success: true, status: 201, message: '', response: resp[0], total_count: resp[1]})
+            return res.json({ success: true, status: 201, message: '', response: resp[0], total_count: resp[1][0].total_count})
         } else {
             return res.json({ success: false, status:500, message: 'Internal server error', response: []})
         }
--- src/api/services/getOrderListService.js
+++ src/api/services/getOrderListService.js
@@ -3,6 +3,10 @@ const readPool = require('../../config/mysql').readPool;
 exports.getOrderList = async (data) => {
     try {
         let searchCondition = ''
+        let searchCondition2 = ''
+        if(data.order_offset) {
+            searchCondition2 = "OFFSET " + data.order_offset + ""
+        }
         if (data.search_criteria) {
             if (data.sbu_id == 0) {
                 searchCondition = "WHERE o.enquiry_number LIKE '%" + data.search_criteria + "%' OR c.customer LIKE '%" + data.search_criteria + "%' OR o.po_number LIKE '%" + data.search_criteria + "%' OR pot.po_type_name LIKE '%" + data.search_criteria + "%' OR o.basic_po_value LIKE '%" + data.search_criteria + "%' OR o.total_po_value LIKE '%" + data.search_criteria + "%'"
@@ -13,17 +17,23 @@ exports.getOrderList = async (data) => {
         }
 
         if (data.sbu_id == 0) {
-            let sql = "SELECT o.enquiry_id, o.enquiry_number, o.order_id, o.sales_person_id, sp.sales_person_name, o.sbu_id, sb.sbu_name, o.customer_id, c.customer, o.po_number, DATE_FORMAT(o.po_date, '%Y-%m-%d') as po_date, o.po_type_id, pot.po_type_name, o.product, o.supply_description, o.brand, o.sitc_description, o.cable_assembly, o.panel, o.welding_receptable, o.clamps, o.hsa_box, o.others, o.basic_po_value, o.total_po_value, DATE_FORMAT(o.scheduled_completion_date, '%Y-%m-%d') as scheduled_completion_date, DATE_FORMAT(o.actual_completion_date, '%Y-%m-%d') as actual_completion_date, o.purchase_order_file, o.completion_file, o.credential_file FROM orders o JOIN po_type pot ON pot.po_type_id = o.po_type_id JOIN sales_person sp ON sp.sales_person_id = o.sales_person_id JOIN sbu sb ON o.sbu_id = sb.sbu_id JOIN customer c ON c.customer_id = o.customer_id " + searchCondition + " ORDER BY o.order_id DESC"
-
+            let sql = "SELECT o.enquiry_id, o.enquiry_number, o.order_id, o.sales_person_id, sp.sales_person_name, o.sbu_id, sb.sbu_name, o.customer_id, c.customer, o.po_number, DATE_FORMAT(o.po_date, '%Y-%m-%d') as po_date, o.po_type_id, pot.po_type_name, o.product, o.supply_description, o.brand, o.sitc_description, o.cable_assembly, o.panel, o.welding_receptable, o.clamps, o.hsa_box, o.others, o.basic_po_value, o.total_po_value, DATE_FORMAT(o.scheduled_completion_date, '%Y-%m-%d') as scheduled_completion_date, DATE_FORMAT(o.actual_completion_date, '%Y-%m-%d') as actual_completion_date, o.purchase_order_file, o.completion_file, o.credential_file FROM orders o JOIN po_type pot ON pot.po_type_id = o.po_type_id JOIN sales_person sp ON sp.sales_person_id = o.sales_person_id JOIN sbu sb ON o.sbu_id = sb.sbu_id JOIN customer c ON c.customer_id = o.customer_id " + searchCondition + " ORDER BY o.order_id DESC LIMIT 10 " + searchCondition2 + ""
             const [resp] = await readPool.query(sql)
-            const total_count = resp.length
-            return [resp, total_count]
-        } else {
-            let sql = "SELECT o.enquiry_id, o.enquiry_number, o.order_id, o.sales_person_id, sp.sales_person_name, o.sbu_id, sb.sbu_name, o.customer_id, c.customer, o.po_number, DATE_FORMAT(o.po_date, '%Y-%m-%d') as po_date, o.po_type_id, pot.po_type_name, o.product, o.supply_description, o.brand, o.sitc_description, o.cable_assembly, o.panel, o.welding_receptable, o.clamps, o.hsa_box, o.others, o.basic_po_value, o.total_po_value, DATE_FORMAT(o.scheduled_completion_date, '%Y-%m-%d') as scheduled_completion_date, DATE_FORMAT(o.actual_completion_date, '%Y-%m-%d') as actual_completion_date, o.purchase_order_file, o.completion_file, o.credential_file FROM orders o JOIN po_type pot ON pot.po_type_id = o.po_type_id JOIN sales_person sp ON sp.sales_person_id = o.sales_person_id JOIN sbu sb ON o.sbu_id = sb.sbu_id JOIN customer c ON c.customer_id = o.customer_id WHERE o.sales_person_id = ? " + searchCondition + " ORDER BY o.order_id DESC"
 
+            // const total_count = resp.length
+            let total_count_sql = "SELECT count(o.order_id) as total_count FROM orders o JOIN po_type pot ON pot.po_type_id = o.po_type_id JOIN sales_person sp ON sp.sales_person_id = o.sales_person_id JOIN sbu sb ON o.sbu_id = sb.sbu_id JOIN customer c ON c.customer_id = o.customer_id " + searchCondition + ""
+            const [total_count_resp] = await readPool.query(total_count_sql);
+
+            return [resp, total_count_resp]
+        } else {
+            let sql = "SELECT o.enquiry_id, o.enquiry_number, o.order_id, o.sales_person_id, sp.sales_person_name, o.sbu_id, sb.sbu_name, o.customer_id, c.customer, o.po_number, DATE_FORMAT(o.po_date, '%Y-%m-%d') as po_date, o.po_type_id, pot.po_type_name, o.product, o.supply_description, o.brand, o.sitc_description, o.cable_assembly, o.panel, o.welding_receptable, o.clamps, o.hsa_box, o.others, o.basic_po_value, o.total_po_value, DATE_FORMAT(o.scheduled_completion_date, '%Y-%m-%d') as scheduled_completion_date, DATE_FORMAT(o.actual_completion_date, '%Y-%m-%d') as actual_completion_date, o.purchase_order_file, o.completion_file, o.credential_file FROM orders o JOIN po_type pot ON pot.po_type_id = o.po_type_id JOIN sales_person sp ON sp.sales_person_id = o.sales_person_id JOIN sbu sb ON o.sbu_id = sb.sbu_id JOIN customer c ON c.customer_id = o.customer_id WHERE o.sales_person_id = ? " + searchCondition + " ORDER BY o.order_id DESC LIMIT 10 " + searchCondition2 + ""
             const [resp] = await readPool.query(sql, [data.sales_person_id]);
-            const total_count = resp.length
-            return [resp, total_count]
+
+            // const total_count = resp.length
+            let total_count_sql = "SELECT count (o.order_id) as total_count FROM orders o JOIN po_type pot ON pot.po_type_id = o.po_type_id JOIN sales_person sp ON sp.sales_person_id = o.sales_person_id JOIN sbu sb ON o.sbu_id = sb.sbu_id JOIN customer c ON c.customer_id = o.customer_id WHERE o.sales_person_id = ? " + searchCondition + ""
+            const [total_count_resp] = await readPool.query(total_count_sql);
+
+            return [resp, total_count_resp]
         }
     } catch (error) {
         console.log('Get order list service error: ', error);
