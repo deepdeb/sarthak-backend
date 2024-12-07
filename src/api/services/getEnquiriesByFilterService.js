a0b1a9bdfcd10e59b7ee610726766a1d484ac779
deepdeb <debnath.pradeep619@gmail.com>
2024-12-07 14:18:46 +0530

    modify files

--- src/api/services/getEnquiriesByFilterService.js
+++ src/api/services/getEnquiriesByFilterService.js
@@ -21,9 +21,11 @@ exports.getEnquiriesByFilter = async (data) => {
             let sales_person_sql = "SELECT sales_person_id FROM sales_person WHERE sales_person_name = ? AND is_deleted = 0"
             const [sales_person_resp] = await readPool.query(sales_person_sql, [data.filter_by_value]);
 
-            let sql = "SELECT e.enquiry_id, e.sbu_id, e.sales_person_id, sp.sales_person_name, e.reff_number, e.customer_id, c.customer, DATE_FORMAT(e.enquiry_date, '%Y-%m-%d') as enquiry_date, e.enquiry_source, et.enquiry_type_name, est.enquiry_sub_type_name, e.principal_house, DATE_FORMAT(e.offer_date, '%Y-%m-%d') as offer_date, e.basic_value, e.tentative_finalization_month, e.tentative_finalization_year, e.status_initial, e.remarks_initial, e.support_initial FROM enquiry e JOIN sales_person sp ON sp.sales_person_id = e.sales_person_id JOIN customer c ON c.customer_id = e.customer_id JOIN enquiry_type et ON e.enquiry_type_id = et.enquiry_type_id JOIN enquiry_sub_type est ON est.enquiry_sub_type_id = e.enquiry_sub_type_id WHERE e.sales_person_id = ? ORDER BY e.enquiry_id DESC"
+            const sales_person_ids = sales_person_resp.map(person => person.sales_person_id);
 
-            const [resp] = await readPool.query(sql, [sales_person_resp[0].sales_person_id])
+            let sql = "SELECT e.enquiry_id, e.sbu_id, e.sales_person_id, sp.sales_person_name, e.reff_number, e.customer_id, c.customer, DATE_FORMAT(e.enquiry_date, '%Y-%m-%d') as enquiry_date, e.enquiry_source, et.enquiry_type_name, est.enquiry_sub_type_name, e.principal_house, DATE_FORMAT(e.offer_date, '%Y-%m-%d') as offer_date, e.basic_value, e.tentative_finalization_month, e.tentative_finalization_year, e.status_initial, e.remarks_initial, e.support_initial FROM enquiry e JOIN sales_person sp ON sp.sales_person_id = e.sales_person_id JOIN customer c ON c.customer_id = e.customer_id JOIN enquiry_type et ON e.enquiry_type_id = et.enquiry_type_id JOIN enquiry_sub_type est ON est.enquiry_sub_type_id = e.enquiry_sub_type_id WHERE e.sales_person_id IN (?) ORDER BY e.enquiry_id DESC"
+
+            const [resp] = await readPool.query(sql, [sales_person_ids])
             const total_count = resp.length
 
             return [resp, total_count];
