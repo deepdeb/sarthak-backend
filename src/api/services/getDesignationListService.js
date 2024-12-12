d83b758793557cd15f35eebaf861cbda011b7999
deepdeb <debnath.pradeep619@gmail.com>
2024-12-12 13:15:30 +0530

    modify src/api/services/getDesignationListService.js

--- src/api/services/getDesignationListService.js
+++ src/api/services/getDesignationListService.js
@@ -2,7 +2,7 @@ const readPool = require('../../config/mysql').readPool
 
 exports.getDesignationList = async () => {
     try {
-        let sql = "SELECT designation_id, designation_name FROM designation WHERE is_deleted = 0 AND is_active = 1"
+        let sql = "SELECT designation_id, designation_name FROM designation WHERE is_deleted = 0 AND is_active = 1 ORDER BY seniority"
         const [resp] = await readPool.query(sql);
 
         return resp
