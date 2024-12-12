f6aba89ddfd23572176675a075d4b6772ae1cdc6
deepdeb <debnath.pradeep619@gmail.com>
2024-12-12 12:30:01 +0530

    modify files

--- src/api/services/filterOrderCategoryService.js
+++ src/api/services/filterOrderCategoryService.js
@@ -3,13 +3,16 @@ const { readPool } = require('../../config/mysql')
 exports.filterOrderCategory = async (data) => {
     try {
         let select_condition = ''
+        let select_condition_2 = ''
         if(data.sbu_id != 0) {
             select_condition = "AND sales_person_id = " + data.sales_person_id + ""
+            select_condition_2 = "WHERE sales_person_id = " + data.sales_person_id + ""
         }
         if(data.filterby_keyword == 'customer') {
-            let sql = "SELECT distinct customer as name FROM customer WHERE is_deleted = 0 AND is_active = 1";
-            const [resp] = await readPool.query(sql)
-            return resp;
+            let sql1 = "SELECT DISTINCT customer as name FROM customer WHERE is_deleted = 0 AND is_active = 1 AND customer_id IN (SELECT customer_id FROM orders WHERE is_deleted = 0 AND is_active = 1) "+ select_condition +""
+            const [resp1] = await readPool.query(sql1)
+
+            return resp1;
         }
         else if (data.filterby_keyword == 'po_type') {
             let sql = "SELECT distinct po_type_name as name FROM po_type WHERE is_deleted = 0 AND is_active = 1";
