425526bce6ca238e668df7efe19f1ca059a2eac6
deepdeb <debnath.pradeep619@gmail.com>
2025-01-23 11:40:01 +0530

    modify files

--- src/api/controllers/common/getCustomerListBySalesperson.js
+++ src/api/controllers/common/getCustomerListBySalesperson.js
@@ -3,22 +3,23 @@ const getCustomerListBySalespersonService = require('../../services/getCustomerL
 exports.getCustomerListBySalespersonController = async (req, res) => {
     try {
         const getCustomerListBySalespersonData = Joi.object({
+            designation_id: Joi.required(),
             sales_person_id: Joi.required()
         })
-        const {error, value} = getCustomerListBySalespersonData.validate(req.body);
-        if(error) {
+        const { error, value } = getCustomerListBySalespersonData.validate(req.body);
+        if (error) {
             console.log(`Invalid customer list by salesperson data:  ${error.details[0].message}`);
             return res.status(400).json({ success: false, message: error.details[0].message.replace(/["':]/g, '') });
         }
         console.log(`Valid customer list by salesperson data`);
         const resp = await getCustomerListBySalespersonService.getCustomerListBySalesperson(req.body);
-        if(resp) {
+        if (resp) {
             return res.json({ success: true, status: 200, response: resp })
         } else {
             return res.json({ success: false, status: 500, message: 'Internal server error', response: [] })
         }
     } catch (error) {
         console.log('Error: ', error);
-        return res.json({ success: false, status: 400, message: res.message, response: []})
+        return res.json({ success: false, status: 400, message: res.message, response: [] })
     }
 }
--- src/api/services/getCustomerListBySalespersonService.js
+++ src/api/services/getCustomerListBySalespersonService.js
@@ -2,8 +2,14 @@ const readPool = require('../../config/mysql').readPool;
 
 exports.getCustomerListBySalesperson = async (data) => {
     try {
-        let customer_list_by_salesperson_sql = "SELECT customer_id, customer FROM customer WHERE sales_person_id = ?"
-        const [customer_list_by_salesperson_resp] = await readPool.query(customer_list_by_salesperson_sql, [data.sales_person_id]);
+        if(data.designation_id == 1) {
+            let customer_list_by_salesperson_sql = "SELECT customer_id, customer FROM customer"
+            var [customer_list_by_salesperson_resp] = await readPool.query(customer_list_by_salesperson_sql);
+        } else {
+            let customer_list_by_salesperson_sql = "SELECT customer_id, customer FROM customer WHERE sales_person_id = ?"
+            var [customer_list_by_salesperson_resp] = await readPool.query(customer_list_by_salesperson_sql, [data.sales_person_id]);
+        }
+
         if (customer_list_by_salesperson_resp) {
             return customer_list_by_salesperson_resp;
         }
